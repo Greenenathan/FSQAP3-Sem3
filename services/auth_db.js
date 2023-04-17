ae9fa53d252728ac885223d26e982c6d21dd23af
Nathan Greene <nathan.greene@keyin.com>
2023-04-17 20:20:21 -0230

    Fixing db issues

--- services/auth_db.js
+++ services/auth_db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "Jordans DB",
+  database: "FS-DATABASE",
   password: "n6907728",
   port: 5432,
 });
